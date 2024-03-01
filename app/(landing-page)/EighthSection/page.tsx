4015da3409ddaeea2a1e4292cffbd32f51890d7d
dewo952 <kabirkhatri1998@gmail.com>
2024-03-01 20:57:14 +0530

    updates

--- app/(landing-page)/EighthSection/page.tsx
+++ app/(landing-page)/EighthSection/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 const EighthSection = () => {
   return (
     <div>
-      <section className="flex flex-col py-20  items-center justify-center bg-[#28282B]">
-        <div className=" text-center  flex flex-col  items-center justify-center pb-20">
+      <section className="flex flex-col py-20 items-center justify-center bg-[#28282B]">
+        <div className="text-center flex flex-col items-center justify-center pb-20">
           <Image
             src="/images/logo/logo-13.svg"
             alt=""
@@ -14,17 +14,14 @@ const EighthSection = () => {
           />
 
           <div
-            className="text-6xl flex justify-center md:text-6xl  text-white  md:px-20 pb-20 w-2/3
-        "
+            className="text-6xl flex justify-center md:text-6xl text-white md:px-20 pb-20 w-2/3"
           >
-            Try the #1 software in project and product management
+            Empower Your Business with Data-Driven Excellence
           </div>
           <div className="text-xl bg-white p-6 rounded-lg w-40 items-center justify-center flex">
             Get started
           </div>
         </div>
-
-    
       </section>
     </div>
   );
